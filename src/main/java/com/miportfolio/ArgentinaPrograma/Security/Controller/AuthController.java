412aa971d09c063b986ef66fa5d0f033714e7f5e
MartinBinaghi <martin.binaghi01@gmail.com>
2022-08-15 23:04:29 -0300

    update

--- src/main/java/com/miportfolio/ArgentinaPrograma/Security/Controller/AuthController.java
+++ src/main/java/com/miportfolio/ArgentinaPrograma/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://personalportfolioap.web.app")
+@CrossOrigin(origins = "https://backendap1.herokuapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
