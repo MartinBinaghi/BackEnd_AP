6f8cc2384288d730a75c3502d8c7d3fee2c4597d
MartinBinaghi <martin.binaghi01@gmail.com>
2022-08-15 22:07:46 -0300

    fixing CORS errors

--- src/main/java/com/miportfolio/ArgentinaPrograma/Controller/CAboutMe.java
+++ src/main/java/com/miportfolio/ArgentinaPrograma/Controller/CAboutMe.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aboutme")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://personalportfolioap.web.app")
 public class CAboutMe {
     @Autowired
     SAboutMe sAboutMe;
